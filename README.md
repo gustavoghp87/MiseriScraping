fb401d4687591b2607c0b0baddb3579494dee425
gustavoghp87 <ghp.2120@gmail.com>
2020-05-17 17:38:25 -0300

    ok

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 # MiseriScraping
-App de terminal en Python para hacer web scraping sobre el sitio de páginas blancas de Argentina y poder descargar gran cantidad de números telefónicos y sus respectivas direcciones, exportando automáticamente a archivo Excel (.xlsx). Se introduce la calle, la altura mínima de barrido y la altura máxima, opcionalmente se cambia de ciudad (CABA por default). Luego se esperan los resultados. Barre cerca de 100 direcciones por minuto.
-Librerías: requiere beautifulsoup (incluída) y xlsxwriter (instalar globalmente)
+App de terminal en Python para hacer web scraping sobre el sitio de Páginas Blancas de Argentina y poder descargar gran cantidad de números telefónicos y sus respectivas direcciones, exportando automáticamente a archivo Excel (.xlsx). Se introduce la calle, la altura mínima de barrido y la altura máxima, opcionalmente se cambia de ciudad (CABA por default). Luego se esperan los resultados. Barre cerca de 100 direcciones por minuto.
+Respecto a las jurisdicciones, partidos y localidades, se depende de la base de datos de Páginas Blancas y hay que ir probando diferentes maneras. Por ejemplo, en la base de datos no figura "san isidro" sino "buenos aires" pero sí toma "tandil". El programa exporta los códigos postales para evitar problemas.
+Lo mismo decimos para el nombre específico de las calles. Por ejemplo, Páginas Blancas toma "j m moreno" y no "jose maria moreno". Hay que ir probando.
+Librerías: requiere beautifulsoup y xlsxwriter (instalar globalmente)
 
-MiseriScraping is a web scraping app written in Python to seek and download telephone numbers with addresses very fast and automatic.
+MiseriScraping is a web scraping app wroten in Python to seek and download telephone numbers with addresses very fast and automatic.
+
+Require:
+pip3 install bs4
+pip3 install XlsxWriter
+
+
+Instrucciones para Windows:
+1) Descargar python 3 desde https://www.python.org/downloads/release/python-383/
+   Es el archivo Windows x86-64 executable installer
+   Opcionalmente ver si hay alguna versión superior
+2) En la ventana de diálogo de instalación del programa tildar la opción "Agregar Python al Path / Add Python 3.8 to Path"
+3) Instalar
+4) Buscar y abrir "cmd" en la lupa de Windows
+   Opción 2: click derecho en el logo de windows y abrir Windows PowerShell
+   Opción 3: abrir C://Windows/system32/cmd
+5) Tipear: python -m pip install -U pip    y dar enter
+   Tiper:  pip install bs4                 y dar enter
+   Tiper:  pip install xlsxwriter          y dar enter
+   Si ninguna de estas cosas funciona, reiniciar pc y volver a intentar
+6) Poner miseriscraping.py en una carpeta exclusiva
+7) Presionando Shift hacer click derecho en alguna parte vacía de la carpeta y seleccionar "Abrir la ventana de PowerShell aquí"
+8) Tipear: python3 .\miseriscraping.py     y dar enter
+9) El programa puede ser interrumpido en cualquier momento con control+c
+
+
--- miseriscraping.py
+++ miseriscraping.py
@@ -9,9 +9,9 @@ from urllib.error import URLError
 from bs4 import BeautifulSoup
 
 
-print("######## BUEN DÍA ESTIMADO/A ######## \n ### ESTO ES MISERISCRAPING ### \n\nEl usuario ingresa calle de la ciudad de Buenos Aires y las alturas mínima y máxima para obtener los números telefónicos de cada vivienda en paginasblancas.com.ar. Exporta a excel automáticamente; el archivo se encontrará en la misma carpeta donde esté este programa. Enjoy it.-\n")
+print("\n\n\n\n            ######## BUEN DÍA ESTIMADO/A! ######## \n                ### ESTO ES MISERISCRAPING ### \n\n\nEl usuario ingresa calle de la ciudad de Buenos Aires -u otras jurisdicciones de Argentina- y las alturas mínima y máxima para obtener los números telefónicos de cada vivienda en paginasblancas.com.ar. Exporta a excel automáticamente; el archivo se encontrará en la misma carpeta donde esté este programa. Enjoy it.-\n")
 
-calle = input("Calle sin acentos: ").lower().strip()
+calle = input("\nCalle: ").lower().strip()
 
 while (calle.find('ñ') != -1):
 	pos = calle.find('ñ')
@@ -20,6 +20,26 @@ while (calle.find('ñ') != -1):
 while (calle.find(' ') != -1):
 	pos = calle.find(' ')
 	calle = calle[0:pos] + '-' + calle[pos+1:]
+
+while (calle.find('á') != -1):
+	pos = calle.find('á')
+	calle = calle[0:pos] + 'a' + calle[pos+1:]
+
+while (calle.find('é') != -1):
+	pos = calle.find('é')
+	calle = calle[0:pos] + 'e' + calle[pos+1:]
+
+while (calle.find('í') != -1):
+	pos = calle.find('í')
+	calle = calle[0:pos] + 'i' + calle[pos+1:]
+
+while (calle.find('ó') != -1):
+	pos = calle.find('ó')
+	calle = calle[0:pos] + 'o' + calle[pos+1:]
+
+while (calle.find('ú') != -1):
+	pos = calle.find('ú')
+	calle = calle[0:pos] + 'u' + calle[pos+1:]
 
 
 dia = int(time.time())
@@ -54,17 +74,44 @@ if (maxInt < minInt):
 
 cantidad = maxInt - minInt + 1
 
-aceptar = input("\nSe van a rastrillar " +str(cantidad) +" direcciones.\n3 y enter para cambiar de ciudad,\n0 y enter para cancelar,\nENTER PARA CONTINUAR: \n")
+aceptar = input("\n-----> Se van a rastrillar " +str(cantidad) +" direcciones.\n\n3 y enter para cambiar de provincia,\n0 y enter para cancelar,\nENTER PARA CONTINUAR: \n")
 
 if (aceptar == '0'):
 	quit()
 
-ciudad = "caba"
+jurisdiccion = "caba"
 if (aceptar == '3'):
-	ciudad = input("Ciudad sin acentos: ").lower().strip()
-	while (ciudad.find(' ') != -1):
-		pos = ciudad.find(' ')
-		ciudad = ciudad[0:pos] + '-' + ciudad[pos+1:]
+	jurisdiccion = input("\nProvincia: ").lower().strip()
+	while (jurisdiccion.find(' ') != -1):
+		pos = jurisdiccion.find(' ')
+		jurisdiccion = jurisdiccion[0:pos] + '-' + jurisdiccion[pos+1:]
+	while (jurisdiccion.find('ñ') != -1):
+		pos = jurisdiccion.find('ñ')
+		jurisdiccion = jurisdiccion[0:pos] + 'n' + jurisdiccion[pos+1:]
+
+	while (jurisdiccion.find(' ') != -1):
+		pos = jurisdiccion.find(' ')
+		jurisdiccion = jurisdiccion[0:pos] + '-' + jurisdiccion[pos+1:]
+
+	while (jurisdiccion.find('á') != -1):
+		pos = jurisdiccion.find('á')
+		jurisdiccion = jurisdiccion[0:pos] + 'a' + jurisdiccion[pos+1:]
+
+	while (jurisdiccion.find('é') != -1):
+		pos = jurisdiccion.find('é')
+		jurisdiccion = jurisdiccion[0:pos] + 'e' + jurisdiccion[pos+1:]
+
+	while (jurisdiccion.find('í') != -1):
+		pos = jurisdiccion.find('í')
+		jurisdiccion = jurisdiccion[0:pos] + 'i' + jurisdiccion[pos+1:]
+
+	while (jurisdiccion.find('ó') != -1):
+		pos = jurisdiccion.find('ó')
+		jurisdiccion = jurisdiccion[0:pos] + 'o' + jurisdiccion[pos+1:]
+
+	while (jurisdiccion.find('ú') != -1):
+		pos = jurisdiccion.find('ú')
+		jurisdiccion = jurisdiccion[0:pos] + 'u' + jurisdiccion[pos+1:]
 
 
 ##################################################################################################
@@ -75,7 +122,7 @@ for i in range(0, cantidad):
 
 	numero = str(minInt + i)
 
-	direccion = 'http://www.paginasblancas.com.ar/direccion/s/' +calle +'-' +numero +'/' +ciudad
+	direccion = 'http://www.paginasblancas.com.ar/direccion/s/' +calle +'-' +numero +'/' +jurisdiccion
 
 	try:
 		datos = urllib.request.urlopen(direccion).read().decode()
@@ -91,7 +138,7 @@ for i in range(0, cantidad):
 
 	ws.write(0, 0, "DIRECCIONES")
 	ws.write(0, 1, "TELÉFONOS")
-	ws.write(0, 2, "CIUDAD: " +ciudad.upper())
+	ws.write(0, 2, "JURISDICCIÓN: " +jurisdiccion.upper())
 
 	# RASTRILLAJE
 	supertags = soup.findAll("div", {"class": "m-results-business-section info"})
@@ -109,6 +156,16 @@ for i in range(0, cantidad):
 			exc = "A" + str(q)
 			ws.write(exc, sinEspacios)
 
+		# CÓDIGO POSTAL
+		taks = supertag('span')
+		for tak in taks:
+			takStr = str(tak.getText())
+			if (takStr.startswith("(CP:")):
+				print(takStr)
+				excC = "C" + str(q)
+				ws.write(excC, takStr)
+
+
 		# TELEFONOS
 		tags = supertag('input')
 		for tag in tags:
@@ -116,13 +173,18 @@ for i in range(0, cantidad):
 				tagInt = int(tag.get('value'))
 				if (tagInt > 100000):               #evitar otros datos
 					tagStr = str(tagInt)
-					tagStr = tagStr[:4] +"-" +tagStr[4:8] +"-" + tagStr[8:]
-					print(tagStr)
+					tagStrG = tagStr[:2] +"-" +tagStr[2:4] +"-" +tagStr[4:8] +"-" + tagStr[8:]
+					print(tagStrG)
 					excB = "B" + str(q)
-					ws.write(excB, tagStr)
+					ws.write(excB, tagStrG)
+					
+					excE = "E" + str(q)
+					ws.write(excE, tagStr)
+
 					
 			except:
 				aux = i   #esto no hace nada
+
 
 
 ##################################################################################################
@@ -130,7 +192,7 @@ for i in range(0, cantidad):
 
 wb.close()
 
-print("#####################    Exportado a archivo Excel con éxito! ")
+print("\n\n\n#####################            Exportado a archivo Excel con éxito! \n\n\n")
 
 
 
