8efcd6c6155d89e6f98e40b2230beefc65080bd5
gustavoghp87 <ghp.2120@gmail.com>
2020-06-04 05:31:28 -0300

    app from course finished

--- src/src/profile_project/profiles_api/urls.py
+++ src/src/profile_project/profiles_api/urls.py
@@ -8,6 +8,12 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, 'hello-viewset')    # allows href to /api/hello-viewset/
 
+router.register('profile', views.UserProfileViewSet)     # django doesn't need a second parameter
+
+router.register('login', views.LoginViewSet, 'login')
+
+router.register('feed', views.UserProfileFeedViewSet)
+
 urlpatterns = [
 	url(r'^hello-view/', views.HelloApiView.as_view()),
 	url(r'', include(router.urls))                            # viewset
--- src/src/profile_project/profiles_api/views.py
+++ src/src/profile_project/profiles_api/views.py
@@ -4,9 +4,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from rest_framework import status
-from . import serializers
 
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+
+from . import serializers
+from . import models
+from . import permissions
+
+
 
 # Create your views here.
 
@@ -91,11 +102,37 @@ class HelloViewSet(viewsets.ViewSet):
 		return Response({'http_method': 'DELETE'})
 
 
+class UserProfileViewSet(viewsets.ModelViewSet):
+	""" Handles creating, and updating profiles """
+	serializer_class = serializers.UserProfileSerializer
+	queryset = models.UserProfile.objects.all()
+	
+	#authentication
+	authentication_classes = (TokenAuthentication,)
+	permission_classes = (permissions.UpdateOwnProfile,)
+
+	#searcher
+	filter_backends = (filters.SearchFilter,)
+	search_fields = ('name', 'email',)
 
 
+class LoginViewSet(viewsets.ViewSet):
+	""" Checks email and password and returns an auth token """
+	serializer_class = AuthTokenSerializer
+	
+	def create(self, request):
+		""" Uses the ObtainAuthToken APIView to validate and create a token """
+		
+		return ObtainAuthToken().post(request)
 
 
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+	""" Handles creating, reading and updating profile feed items """
+	authentication_classes = (TokenAuthentication,)
+	serializer_class = serializers.ProfileFeedItemSerializer
+	queryset = models.ProfileFeedItem.objects.all()
+	permission_classes = (permissions.PostOwnStatus, IsAuthenticated)
 
-
-
-
+	def perform_create(self, serializers):
+		""" Sets the user profile to the logged in user """
+		serializers.save(user_profile=self.request.user)
